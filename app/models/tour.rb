e556743108130dc459b221989c3ef6a96e46624b
valikos <valikos.ost@gmail.com>
2013-01-11 16:36:08 +0200

    Added price to tour

--- app/models/tour.rb
+++ app/models/tour.rb
@@ -1,8 +1,9 @@
 class Tour < ActiveRecord::Base
-  attr_accessible :title, :description, :active, :start_date, :end_date
+  attr_accessible :title, :description, :price, :active, :start_date, :end_date
 
   validates :title, presence: true
   validates :description, presence: true
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 }
   # validates :start_date, presence: true
   # validates :end_date, presence: true
 end
