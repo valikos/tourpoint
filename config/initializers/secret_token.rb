a065e6b784619fb1e33bf796d077dd4e0f10bbd3
BishOFF <jaguar17171@gmail.com>
2013-01-11 12:46:15 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Tourhop::Application.config.secret_token = '2aa55f65d843f7fa99604ef0ea5ca601866808b1678206e4a9ea100a93200f6a5e6fba02eb7ac8fea9de743da8dcc51536b129e8446ce51b28d0b2a1e696a29f'
