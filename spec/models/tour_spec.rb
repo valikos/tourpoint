bc51cd920efef5641cdbc50a64bac499a0a67559
BishOFF <jaguar17171@gmail.com>
2013-01-11 16:36:08 +0200

    Added price to tour

--- spec/models/tour_spec.rb
+++ spec/models/tour_spec.rb
@@ -5,6 +5,7 @@ describe Tour do
   describe "db table" do
     it { should have_db_column :title }
     it { should have_db_column :description }
+    it { should have_db_column :price }
     it { should have_db_column :active }
     it { should have_db_column :start_date }
     it { should have_db_column :end_date }
@@ -13,6 +14,7 @@ describe Tour do
   describe "validations" do
     it { should validate_presence_of :title }
     it { should validate_presence_of :description }
+    it { should validate_numericality_of :price }
     xit { should validate_presence_of :start_date }
     xit { should validate_presence_of :end_date }
   end
