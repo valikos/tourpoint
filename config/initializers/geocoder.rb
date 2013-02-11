4448fd4c274ebe97b33e59d02ce8ca1deae8e8c9
BishOFF <jaguar17171@gmail.com>
2013-02-11 16:07:38 +0200

    new configs for geocoder

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,16 +1,26 @@
-# geocoding service (see below for supported options):
-Geocoder::Configuration.lookup = :maxmind
-Geocoder::Configuration.ip_lookup = :maxmind
+# # geocoding service (see below for supported options):
+# Geocoder::Configuration.lookup = :maxmind
+# Geocoder::Configuration.ip_lookup = :ip_maxmind
 
-# to use an API key:
-# FIXME:Need to move this to a different key in production
-Geocoder::Configuration.api_key = ENV["MAXMIND_GEOIP_CITY"]
+# # to use an API key:
+# # FIXME:Need to move this to a different key in production
+# Geocoder::Configuration.api_key = ENV["MAXMIND_GEOIP_CITY"]
 
-# geocoding service request timeout, in seconds (default 3):
-Geocoder::Configuration.timeout = 10
+# # geocoding service request timeout, in seconds (default 3):
+# Geocoder::Configuration.timeout = 10
 
-# use HTTPS for geocoding service connections:
-Geocoder::Configuration.use_https = false
+# # use HTTPS for geocoding service connections:
+# Geocoder::Configuration.use_https = false
 
-# language to use (for search queries and reverse geocoding):
-Geocoder::Configuration.language = :en
+# # language to use (for search queries and reverse geocoding):
+# Geocoder::Configuration.language = :en
+
+Geocoder.configure(
+  :lookup => :bing,
+  :ip_lookup => :maxmind,
+  :bing => { :api_key => ENV["BING_KEY"] },
+  :maxmind => { :api_key => ENV["MAXMIND_GEOIP_CITY"], :service => :city },
+  :timeout => 10,
+  :use_https => false,
+  :language => :en
+)
